0a7b2abc380bd09761f988ab317196cecf3219df
axix152 <axixkhanii46@gmail.com>
2022-06-19 01:20:17 +0500

    second commit

--- lib/global.dart
+++ lib/global.dart
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-=======
 import 'package:event_planner/models/userModel.dart';
->>>>>>> second commit
 import 'package:firebase_auth/firebase_auth.dart';
 
 final FirebaseAuth fauth = FirebaseAuth.instance;
 
 User? currentFirebaseUser;
-<<<<<<< HEAD
-=======
 UserModel? userModel;
->>>>>>> second commit
